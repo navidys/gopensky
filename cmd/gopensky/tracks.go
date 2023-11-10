9bce55c2f15f04cdfc8bfbf66135a01f8b2b8e86
Navid Yaghoobi <navidys@fedoraproject.org>
2023-11-10 13:58:23 +1100

    feature - tracks by aircraft
    Signed-off-by: Navid Yaghoobi <navidys@fedoraproject.org>

--- cmd/gopensky/tracks.go
+++ cmd/gopensky/tracks.go
@@ -17,8 +17,7 @@ func registerTracksCommand() {
 	// flights command
 	tracksCommand.Flags().Int64VarP(&cmdTime, "time", "t", cmdTime,
 		`unix time in seconds since epoch. It can be any time between start and end of a known flight
-		 If time = 0, get the live track if there is any flight ongoing for the given aircraft.
-		`)
+If time = 0, get the live track if there is any flight ongoing for the given aircraft.`)
 
 	tracksCommand.Flags().StringVarP(&cmdAircraft, "aircraft", "a", cmdAircraft,
 		"unique ICAO 24-bit address of the transponder in hex string representation.")
