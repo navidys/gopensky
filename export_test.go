41bb435f3e51ac6a2f1e187da637d6ec044926d3
Navid Yaghoobi <navidys@fedoraproject.org>
2023-11-16 18:47:04 +1100

    unit tests
    Signed-off-by: Navid Yaghoobi <navidys@fedoraproject.org>

--- export_test.go
+++ export_test.go
@@ -1,8 +1,9 @@
 package gopensky
 
 var (
+	DecodeRawStateVector    = decodeRawStateVector
 	FloatToString           = floatToString
 	GetFlightsRequestParams = getFlightsRequestParams
+	GetTracksRequestParams  = getTracksRequestParams
 	GetStateRequestParams   = getStateRequestParams
-	DecodeRawStateVector    = decodeRawStateVector
 )
--- tracks_test.go
+++ tracks_test.go
@@ -2,6 +2,7 @@ package gopensky_test
 
 import (
 	"context"
+	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -20,6 +21,32 @@ var _ = Describe("Tracks", func() {
 
 			_, err = gopensky.GetTrackByAircraft(conn, "a835af", -1)
 			Expect(err).To(Equal(gopensky.ErrInvalidUnixTime))
+		})
+	})
+
+	Describe("getTracksRequestParams", func() {
+		It("generate track request parameters", func() {
+			tests := []struct {
+				time   int64
+				icao24 string
+			}{
+				{time: -1, icao24: "icao24_a"},
+				{time: -1, icao24: ""},
+				{time: 0, icao24: "icao24_b"},
+				{time: 2, icao24: "icao24_c"},
+			}
+
+			for _, reqParams := range tests {
+				urlVal := gopensky.GetTracksRequestParams(reqParams.time, reqParams.icao24)
+				Expect(urlVal.Get("icao24")).To(Equal(reqParams.icao24))
+
+				if reqParams.time >= 0 {
+					reqTime := fmt.Sprintf("%d", reqParams.time)
+					Expect(urlVal.Get("time")).To(Equal(reqTime))
+				} else {
+					Expect(urlVal.Get("time")).To(Equal(""))
+				}
+			}
 		})
 	})
 })
