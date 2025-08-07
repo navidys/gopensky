1da91b7807ba1695731b53f8da4d4a05ea5064e1
Navid Yaghoobi <navidys@fedoraproject.org>
2025-08-07 10:30:36 +1000

    code improvement
    Signed-off-by: Navid Yaghoobi <navidys@fedoraproject.org>

--- states.go
+++ states.go
@@ -6,6 +6,146 @@ import (
 	"net/url"
 )
 
+const (
+	stateVecIaco24Index = 0 + iota
+	stateVecCallsignIndex
+	stateVecCountryIndex
+	stateVecTimePositionIndex
+	stateVecLastContactIndex
+	stateVecLongitudeIndex
+	stateVecLatitudeIndex
+	stateVecBaroAltitudeIndex
+	stateVecOnGroundIndex
+	stateVecVelocityIndex
+	stateVecTrueTrackIndex
+	stateVecVerticalRateIndex
+	stateVecSensorsIndex
+	stateVecGeoAltitudeIndex
+	stateVecSquawkIndex
+	stateVecSpiIndex
+	stateVecPositionSourceIndex
+	stateVecCategoryIndex
+)
+
+type StatesResponse struct {
+	// The time which the state vectors in this response are associated with.
+	// All vectors represent the state of a vehicle with the interval.
+	Time int64 `json:"time"`
+
+	// The state vectors.
+	States [][]interface{} `json:"states"`
+}
+
+type States struct {
+	// The time which the state vectors in this response are associated with.
+	// All vectors represent the state of a vehicle with the interval.
+	Time int64 `json:"time"`
+
+	// The state vectors.
+	// States []StateVector `json:"states"`
+	States []StateVector `json:"states"`
+}
+
+type StateVector struct {
+	// Unique ICAO 24-bit address of the transponder in hex string representation.
+	Icao24 string `json:"icao24"`
+
+	// Callsign of the vehicle (8 chars). Can be nil if no callsign has been received.
+	Callsign *string `json:"callsign"`
+
+	// Country name inferred from the ICAO 24-bit address.
+	OriginCountry string `json:"originCountry"`
+
+	// Unix timestamp (seconds) for the last position update.
+	// Can be nil if no position report was received by OpenSky within the past 15s.
+	TimePosition *int64 `json:"timePosition"`
+
+	// Unix timestamp (seconds) for the last update in general.
+	//  This field is updated for any new, valid message received from the transponder.
+	LastContact int64 `json:"lastContact"`
+
+	// WGS-84 longitude in decimal degrees. Can be nil.
+	Longitude *float64 `json:"longitude"`
+
+	// WGS-84 latitude in decimal degrees. Can be nil.
+	Latitude *float64 `json:"latitude"`
+
+	// Barometric altitude in meters. Can be nil.
+	BaroAltitude *float64 `json:"baroAltitude"`
+
+	// Boolean value which indicates if the position was retrieved from a surface position report.
+	OnGround bool `json:"onGround"`
+
+	// Velocity over ground in m/s. Can be nil.
+	Velocity *float64 `json:"velocity"`
+
+	// True track in decimal degrees clockwise from north (north=0°). Can be nil.
+	TrueTrack *float64 `json:"trueTrack"`
+
+	// Vertical rate in m/s.
+	// A positive value indicates that the airplane is climbing, a negative value indicates that it descends.
+	// Can be nil.
+	VerticalRate *float64 `json:"verticalTate"`
+
+	// IDs of the receivers which contributed to this state vector.
+	//  Is nil if no filtering for sensor was used in the request.
+	Sensors []int `json:"sensors"`
+
+	// Geometric altitude in meters. Can be nil.
+	GeoAltitude *float64 `json:"geoAltitude"`
+
+	// The transponder code aka Squawk. Can be nil.
+	Squawk *string `json:"squawk"`
+
+	// Whether flight status indicates special purpose indicator.
+	Spi bool `json:"spi"`
+
+	// Origin of this state’s position.
+	// 0 = ADS-B
+	// 1 = ASTERIX
+	// 2 = MLAT
+	// 3 = FLARM
+	PositionSource int `json:"positionSource"`
+
+	// Aircraft category.
+	// 0 = No information at all
+	// 1 = No ADS-B Emitter Category Information
+	// 2 = Light (< 15500 lbs)
+	// 3 = Small (15500 to 75000 lbs)
+	// 4 = Large (75000 to 300000 lbs)
+	// 5 = High Vortex Large (aircraft such as B-757)
+	// 6 = Heavy (> 300000 lbs)
+	// 7 = High Performance (> 5g acceleration and 400 kts)
+	// 8 = Rotorcraft
+	// 9 = Glider / sailplane
+	// 10 = Lighter-than-air
+	// 11 = Parachutist / Skydiver
+	// 12 = Ultralight / hang-glider / paraglider
+	// 13 = Reserved
+	// 14 = Unmanned Aerial Vehicle
+	// 15 = Space / Trans-atmospheric vehicle
+	// 16 = Surface Vehicle – Emergency Vehicle
+	// 17 = Surface Vehicle – Service Vehicle
+	// 18 = Point Obstacle (includes tethered balloons)
+	// 19 = Cluster Obstacle
+	// 20 = Line Obstacle
+	Category int `json:"category"`
+}
+
+type BoundingBoxOptions struct {
+	// Lower bound for the latitude in decimal degrees.
+	Lamin float64
+
+	// lower bound for the longitude in decimal degrees.
+	Lomin float64
+
+	// upper bound for the latitude in decimal degrees.
+	Lamax float64
+
+	// upper bound for the longitude in decimal degrees.
+	Lomax float64
+}
+
 // Retrieve state vectors for a given time. If time = 0 the most recent ones are taken.
 // It is possible to query a certain area defined by a bounding box of WGS84 coordinates.
 // You can request the category of aircraft by setting extended to true.
@@ -256,4 +396,14 @@ func decodeRawStateVector(data []interface{}) (*StateVector, error) { //nolint:f
 	}
 
 	return &stVector, nil
+}
+
+// NewBoundingBox returns new bounding box options for states information gathering.
+func NewBoundingBox(lamin float64, lomin float64, lamax float64, lomax float64) *BoundingBoxOptions {
+	return &BoundingBoxOptions{
+		Lamin: lamin,
+		Lomin: lomin,
+		Lamax: lamax,
+		Lomax: lomax,
+	}
 }
